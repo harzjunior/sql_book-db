686d0bdc023c3e82d47d550bbb6b61a8c21660fb
harzjunior <harunjibs@gmail.com>
2024-03-07 18:18:37 +0100

    Add Book Feature

--- appOne.js
+++ appOne.js
@@ -40,11 +40,38 @@ app.get("/books", (req, res) => {
   });
 });
 
+// Add this route to handle the POST request for adding a new book
+app.post("/books", (req, res) => {
+  const { title, author, email } = req.body;
+  const query =
+    "INSERT INTO books (book_title, author_name, email) VALUES (?, ?, ?)";
+
+  // Execute the query
+  db.query(query, [title, author, email], (error, results) => {
+    if (error) {
+      console.error("Error adding new book to MySQL:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      // Retrieve the updated book list
+      const selectQuery = "SELECT * FROM books";
+      db.query(selectQuery, (selectError, selectResults) => {
+        if (selectError) {
+          console.error("Error fetching data from MySQL:", selectError);
+          res.status(500).json({ error: "Internal Server Error" });
+        } else {
+          res.json(selectResults);
+        }
+      });
+    }
+  });
+});
+
 // Serve the HTML file
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+//listening on http://localhost:3000/
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
